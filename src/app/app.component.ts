7cc67a5ffdfe12644e4b3f721f20ae99e5df4f0c
myusuf <myusufozcelik96@gmail.com>
2023-06-15 13:32:16 +0300

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,8 +3,57 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
-  title = 'password';
+  
+  password = '';
+  length = 0;
+  isIncludeLetters = false;
+  isIncludeNumbers = false;
+  isIncludeSymbols = false;
+
+  modifyLength(value: number) {
+    this.length = value;
+  }
+
+  modifyLetters() {
+    this.isIncludeLetters = !this.isIncludeLetters;
+  }
+
+  modifyNumbers() {
+    this.isIncludeNumbers = !this.isIncludeNumbers;
+  }
+
+  modifySymbols() {
+    this.isIncludeSymbols = !this.isIncludeSymbols;
+  }
+
+  buttonClick() {
+
+    if (!this.isIncludeLetters && !this.isIncludeNumbers && !this.isIncludeSymbols) 
+    return;
+
+    const numbers = '1234567890';
+    const letters = 'abcdefghijklmnopqrstuvwxyz';
+    const symbols = '!@#$%^&*()_+-={}[]:";\'<>?,./|\\';
+
+    let validChars = '';
+
+    if (this.isIncludeLetters) validChars += letters;
+
+    if (this.isIncludeNumbers) validChars += numbers;
+
+    if (this.isIncludeSymbols) validChars += symbols;
+
+    let generatedPassword = '';
+
+    for (let i = 0; i < this.length; i++) {
+      const index = Math.floor(Math.random() * validChars.length);
+      generatedPassword += validChars[index];
+    }
+
+    this.password = generatedPassword;
+
+  }
 }
